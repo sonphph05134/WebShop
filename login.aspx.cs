3bb7088725cf40c95349ef3040027aab2667ca44
Tuandao1998 <tuanchiko1998@gmail.com>
2018-04-27 14:49:54 +0700

    Tuan lam trang login

---
+++ login.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["USERNAME"] == null)
+            {
+                Response.Redirect("account.aspx");
+
+            }
+        }
+
+    
+}
